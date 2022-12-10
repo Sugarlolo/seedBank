980a612e0d98d7b3dce82c38dd5df31317fd14c9
wonsuk <jyunjyong3@gamil.com>
2022-12-10 23:38:20 +0900

    검색 쿼리문 수정

--- src/VIEW/seed_search_write.java
+++ src/VIEW/seed_search_write.java
@@ -111,8 +111,7 @@ public class seed_search_write extends JFrame {
 				String FromStr = tfFrom.getText();
 				String checksearch = cb_list.getSelectedItem().toString();
 				// PreparedStatement 사용
-				 
-				String sql = "select 자원명,수량,원산지,수집지,자원구분,평균수명 from 종자 where 자원명 = (?) and 원산지 = (?) and 자원구분 (?)";
+				String sql = "select 자원명,수량,원산지,수집지,자원구분,평균수명 from 종자 where 자원명 = (?) and 원산지 = (?) and 자원구분 = (?)";
 				try {
 					PreparedStatement pstmt = db.getConnection().prepareStatement(sql);
 					pstmt.setString(1,SnameStr);
